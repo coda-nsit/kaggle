c80f310e33ad4ddbf832ee242370dbe96e2caeca
Rishab Banerjee <crpbanerjee@gmail.com>
2020-04-06 22:56:24 -0700

    get the k most similar tokens to seed tokens. Fully working code.

--- similarity-from-embeddings.py
+++ similarity-from-embeddings.py
@@ -1,3 +1,9 @@
+"""
+python3 similarity-from-embeddings.py --data_dir word_embeddings/ --distance_metric cosine --k_similar 10 --output_dir similar_words
+
+no need to run this file as a sbatch command. It doesn't make use of GPU
+"""
+
 import argparse
 import glob
 import pickle
@@ -22,6 +28,14 @@ def get_embeddings_for_seed_words(file_name):
     return pickle.load(f)
 
 
+# returns the distance of <token> from all the seed words
+def distance_from_seeds(distance_metric, token, seed_to_embedding, token_to_embedding):
+  dist = 0
+  for seed, seed_embedding in seed_to_embedding.items():
+    dist += distance_metric(seed_embedding.reshape(1, -1), token_to_embedding[token].reshape(1, -1))
+  return dist
+
+
 def main():
   parser = argparse.ArgumentParser()
 
@@ -31,6 +45,13 @@ def main():
     type=str,
     required=True,
     help="The directory storing the word embeddings of the tokens (as python dictionary {token : embedding}) in pickle format")
+
+  parser.add_argument(
+    "--output_dir",
+    default="similar_words/",
+    type=str,
+    required=True,
+    help="The directory storing the word embeddings of the tokens which are most similar (as python dictionary {token : embedding}) in pickle format")
 
   parser.add_argument(
     "--distance_metric",
@@ -57,9 +78,11 @@ def main():
   token_to_embedding_pq_words = {}
 
   token_to_embeddings_files = glob.glob(args.data_dir + "word_embeddings_*.pickle")
-  seed_to_embeddings = get_embeddings_for_seed_words(os.path.join(args.data_dir, "seed_embeddings_.pickle"))
+  seed_to_embedding = get_embeddings_for_seed_words(os.path.join(args.data_dir, "seed_embeddings_.pickle"))
 
   pq = PriorityQueue(k)
+
+  logger.info("seed tokens are %s", str(list(seed_to_embedding.keys())))
 
   '''
   for every token:
@@ -70,30 +93,34 @@ def main():
     with open(token_to_embeddings_file, "rb") as f:
       token_to_embedding = pickle.load(f)
 
-    for token, embedding in token_to_embedding.items():
+    for token, embedding in list(token_to_embedding.items()):
+      # don't consider seed words
+      if token in seed_to_embedding:
+        continue
+
       if pq.full() is True:
-        most_far_word = pq.get()
-        most_far_word_embedding = token_to_embedding_pq_words[most_far_word]
+        most_far_word_distance, most_far_word = pq.get()
         token_to_embedding_pq_words.pop(most_far_word)
 
-        cos_1 = cos_2 = 0
-
-        for seed, seed_embedding in seed_to_embeddings:
-          cos_1 += find_distance(distance_metric, seed_embedding, most_far_word_embedding)
-          cos_2 += find_distance(distance_metric, seed_embedding, embedding)
-
-        if cos_1 > cos_2:
-          pq.put(most_far_word)
-          token_to_embedding_pq_words[most_far_word] = most_far_word_embedding
+        dist_current_token = distance_from_seeds(distance_metric, token, seed_to_embedding, token_to_embedding)
+        if dist_current_token > most_far_word_distance:
+          pq.put((most_far_word_distance, most_far_word))
+          token_to_embedding_pq_words[most_far_word] = token_to_embedding[most_far_word]
         else:
-          pq.put(token)
+          pq.put((dist_current_token, token))
           token_to_embedding_pq_words[token] = embedding
 
       else:
-        pq.put(token)
+        dist = distance_from_seeds(distance_metric, token, seed_to_embedding, token_to_embedding)
+        pq.put((dist, token))
         token_to_embedding_pq_words[token] = embedding
 
-  logger.info("closest tokens %s", str(token_to_embedding_pq_words.keys()))
+  with open(f'{args.output_dir}/closest_word_to_embeddings.pickle', 'wb') as handle:
+    pickle.dump(token_to_embedding_pq_words, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+  while pq.empty() is False:
+    dist, word = pq.get()
+    logger.info("%s %f", word, dist)
 
 if __name__ == "__main__":
   main()
