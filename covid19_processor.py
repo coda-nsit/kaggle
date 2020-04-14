3f2c2732bc97cc419f9df3d895d0a5a8bb3d472d
Rishab Banerjee <crpbanerjee@gmail.com>
2020-04-13 22:40:27 -0700

    fully working code for finetuning BertForSequenceClassification including preprocessing code; f1=0.92

--- covid19_processor.py
+++ covid19_processor.py
@@ -3,9 +3,13 @@ import json
 import logging
 import os
 from collections import defaultdict
-from sklearn.model_selection import train_test_split
+import pickle
 
-from torch.utils.data import Dataset, DataLoader, SequentialSampler
+
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -18,26 +22,25 @@ class PaperAbstractDataset(Dataset):
   """
   returns the paper_id np array, input_ids tensor and attention_mask tensor
   """
-
-  def __init__(self, dataset):
-    self.dataset = dataset
+  def __init__(self, paper_ids, input_ids, attention_masks, labels):
+    self.input_ids = input_ids
+    self.attention_masks = attention_masks
+    self.labels = labels
+    self.paper_ids = paper_ids
 
   def __getitem__(self, index):
-    paper_id = self.dataset[index]["paper_id"]
-    input_id = self.dataset[index]["input_id"]
-    attention_mask = self.dataset[index]["attention_mask"]
-    return paper_id, input_id, attention_mask
+    return self.paper_ids[index], self.input_ids[index], self.attention_masks[index], self.labels[index]
 
   def __len__(self):
-    return self.dataset.shape[0]
+    assert len(self.paper_ids) == self.input_ids.shape[0] == self.attention_masks.shape[0] == len(self.labels)
+    return len(self.paper_ids)
 
 
 class Covid19Processor(object):
   def __init__(self):
     pass
 
-
-  def get_vaccine_and_therapeutic_paper_ids(self, only_vaccine_file, only_therapeutic_file):
+  def get_vaccine_and_therapeutic_paper_ids(self, filter_files_dir, only_vaccine_file, only_therapeutic_file):
     """
     only_vaccine_file: a file containing a list of paper ids that only contain vaccine keywords
     only_therapeutic_file: a file containing a list of paper ids that only contain therapeutic keywords
@@ -46,12 +49,11 @@ class Covid19Processor(object):
     only_vaccine_file_names = set()
     only_therapeutic_file_names = set()
 
-    with open(only_vaccine_file, "r") as f:
-      only_vaccine_file_names.update(set([paper_id for paper_id in f.readlines()]))
-    with open(only_therapeutic_file_names, "r") as f:
-      only_therapeutic_file_names.update(set([paper_id for paper_id in f.readlines()]))
+    with open(os.path.join(filter_files_dir, only_vaccine_file), "r") as f:
+      only_vaccine_file_names.update(set([paper_id.rstrip("\n") for paper_id in f.readlines()]))
+    with open(os.path.join(filter_files_dir, only_therapeutic_file), "r") as f:
+      only_therapeutic_file_names.update(set([paper_id.rstrip("\n") for paper_id in f.readlines()]))
     return only_vaccine_file_names, only_therapeutic_file_names
-
 
   def extract_json_file_names(self, data_dir, filter_dir):
     json_file_names = glob.glob(f"{data_dir}/biorxiv_medrxiv/pdf_json/*.json")
@@ -67,15 +69,22 @@ class Covid19Processor(object):
       filter_set = set()
       for filter_file_name in filter_file_names:
         with open(filter_file_name, "r") as f:
-          filter_set.update(set([paper_id + ".json" for paper_id in f.readlines()]))
+          filter_set.update(set([paper_id.rstrip("\n") + ".json" for paper_id in f.readlines()]))
+
       json_file_names = list(filter(lambda x: os.path.split(x)[1] in filter_set, json_file_names))
 
     logger.info(" sample json file name: %s", str(json_file_names[0]))
     logger.info(" total json files available in dataset: %d", len(json_file_names))
     return json_file_names
 
+  def preprocess_data_to_df(self, json_files, only_vaccine_paper_ids, only_therapeutic_paper_ids):
+    print("only_vaccine_paper_ids")
+    print(only_vaccine_paper_ids)
+    print("\n\n")
+    print("only_vaccine_paper_ids")
+    print(only_vaccine_paper_ids)
+    print("\n\n")
 
-  def preprocess_data_to_df(self, json_files, only_vaccine_file_name, only_therapeutic_file_name):
     empty_abstract_file_names = []
     empty_body_file_names = []
     paper_data = defaultdict(lambda: defaultdict(list))
@@ -129,86 +138,63 @@ class Covid19Processor(object):
       data.append((paper_id,
                    paper_data[paper_id]["abstract"],
                    paper_data[paper_id]["body_text"]))
-      labels.append(0 if paper_id in only_vaccine_file_name else 1)
+      labels.append(0 if paper_id in only_vaccine_paper_ids else 1)
 
     logger.info(" total valid abstracts: %s", valid_abstracts)
     logger.info(" empty_abstract_file_names: %d", len(empty_abstract_file_names))
     logger.info(" empty_body_file_names %d", len(empty_body_file_names))
     data = pd.DataFrame(data, columns=["paper_id", "abstract", "body_text"])
+    labels = pd.DataFrame(labels, columns=["label"])
     logger.info(" shape of data: %s", str(data.shape))
     return train_test_split(data, labels, test_size=0.33)
 
+  def create_input_ids__attention_masks_tensor(self, X_train, y_train, tokenizer, max_seq_length):
+    # Tokenize all of the sentences and map the tokens to their word IDs.
+    abstracts = X_train["abstract"].values
+    paper_ids = X_train["paper_id"].values
+    labels = y_train["label"].values
 
+    paper_ids_with_abstracts = []
+    labels_with_abstract = []
+    input_ids = []
+    attention_masks = []
 
-  def get_dev_examples(self, data_dir, filename):
-    """
-    Returns the evaluation example from the data directory.
-    Args:
-    data_dir: Directory containing the data files used for training and evaluating.
-    filename: the evaluation dataset filename.
-    """
-    if data_dir is None:
-      data_dir = ""
+    for idx, (paper_id, point, label) in enumerate(zip(paper_ids, abstracts, labels)):
+      if len(point) == 0:
+        continue
 
-    with open(os.path.join(data_dir, filename), "r", encoding="utf-8") as reader:
-      input_data = json.load(reader)["data"]
-    return self.create_examples(input_data, "dev")
+      # `encode_plus` will:
+      #   (1) Tokenize the sentence.
+      #   (2) Prepend the `[CLS]` token to the start.
+      #   (3) Append the `[SEP]` token to the end.
+      #   (4) Map tokens to their IDs.
+      #   (5) Pad or truncate the sentence to `max_length`
+      #   (6) Create attention masks for [PAD] tokens.
+      encoded_dict = tokenizer.encode_plus(
+        point,  # Sentence to encode.
+        add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
+        max_length=max_seq_length,  # Pad & truncate all sentences.
+        pad_to_max_length=True,
+        return_attention_mask=True,  # Construct attn. masks.
+        return_tensors='pt',  # Return pytorch tensors.
+      )
 
+      # Add the encoded sentence to the list.
+      input_ids.append(encoded_dict['input_ids'])
 
-  def get_train_examples(self, data_dir, filename):
-    """
-    Returns the training examples from the data directory.
-    Args:
-    data_dir: Directory containing the data files used for training and evaluating.
-    filename: the train dataset filename.
-    """
-    if data_dir is None:
-      data_dir = ""
+      # And its attention mask (simply differentiates padding from non-padding).
+      attention_masks.append(encoded_dict['attention_mask'])
 
-    with open(os.path.join(data_dir, filename), "r", encoding="utf-8") as reader:
-      input_data = json.load(reader)["data"]
-    return self.create_examples(input_data, "train")
+      paper_ids_with_abstracts.append(paper_id)
+      labels_with_abstract.append(label)
 
-
-  def create_examples(self, input_data, set_type):
-    is_training = set_type == "train"
-    examples = []
-    for entry in tqdm(input_data):
-      title = entry["title"]
-      for paragraph in entry["paragraphs"]:
-        context_text = paragraph["context"]
-        for qa in paragraph["qas"]:
-          qas_id = qa["id"]
-          question_text = qa["question"]
-          start_position_character = None
-          answer_text = None
-          answers = []
-
-          if "is_impossible" in qa:
-            is_impossible = qa["is_impossible"]
-          else:
-            is_impossible = False
-
-          if not is_impossible:
-            if is_training:
-              answer = qa["answers"][0]
-              answer_text = answer["text"]
-              start_position_character = answer["answer_start"]
-            else:
-              answers = qa["answers"]
-
-          example = SquadExample(
-            qas_id=qas_id,
-            question_text=question_text,
-            context_text=context_text,
-            answer_text=answer_text,
-            start_position_character=start_position_character,
-            title=title,
-            is_impossible=is_impossible,
-            answers=answers,
-          )
-
-          examples.append(example)
-    return examples
-
-  def convert_examples_to_features(self):
+    # Convert the lists into tensors.
+    input_ids = torch.cat(input_ids, dim=0)
+    attention_masks = torch.cat(attention_masks, dim=0)
+    torch.save(input_ids, 'input_ids.pt')
+    torch.save(attention_masks, 'attention_masks.pt')
+    with open(f'paper_ids.pickle', 'wb') as handle:
+      pickle.dump(paper_ids_with_abstracts, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open(f'labels.pickle', 'wb') as handle:
+      pickle.dump(labels_with_abstract, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    return input_ids, attention_masks, paper_ids_with_abstracts, labels_with_abstract
